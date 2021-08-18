29155e50303b1d416b4217279caa58e51e98e9cd
Romeo Franco Artista <r.artista@tose.com.ph>
2021-08-18 07:58:34 +0000

    Added error handling

--- resources/views/viewer.blade.php
+++ resources/views/viewer.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <h1 class="text-center">Online Document Viewer</h1>
 @php
-$viewer = request()->input('viewer');
+$viewer = strtolower(request()->input('viewer'));
 $type = '';
 
 if ($viewer == 'google') {
@@ -11,7 +11,13 @@ if ($viewer == 'google') {
     $type = 'Microsoft Documents Viewer';
 }
 
-$file = request()->input('file');
+$file = strtolower(request()->input('file'));
+$error = '';
+
+if (!$file) {
+    $error = 'Unrecognized viewer or file type';
+}
+
 $file_url = '';
 
 switch ($viewer) {
@@ -22,15 +28,25 @@ switch ($viewer) {
         $file_url = 'https://view.officeapps.live.com/op/embed.aspx?src=';
         break;
     default:
+        $file_url = '';
         break;
 }
 
-$file_url = $file_url . 'https://github.com/romeoartista-toseph/doc-viewer/raw/master/resources/files/sample.' . $file;
+if (!$file_url) {
+    $error = 'Unrecognized viewer or file type';
+} else {
+    $file_url = $file_url . 'https://github.com/romeoartista-toseph/doc-viewer/raw/master/resources/files/sample.' . $file;
+}
+
 @endphp
+@if ($error)
+<div class="alert alert-danger">{{ $error }}</div>
+@else
 <h2>Viewer: {{ $type }}</h2>
 <h2>File: {{ $file }}</h2>
 <a class="btn btn-primary mb-5" href="{{ route('index') }}">&lt;&lt; Back</a>
 <div class="ratio ratio-1x1">
     <iframe src="{{ $file_url }}" allowfullscreen></iframe>
 </div>
+@endif
 @endsection
